433208d3065272e5f7bea23682d1f22cbd8f7f44
Naveen Tumkur Ramesh Babu <naveentrrko@gmail.com>
2018-01-27 10:09:43 -0500

    python simple program.

---
+++ simple_indentattion.py
@@ -0,0 +1,10 @@
+#Python uses indentation for blocks, instead of curly braces. Both tabs and spaces are supported, but the standard indentation requires standard Python code to use four spaces. For example:
+
+
+x = 2
+if x == 1:
+    # indented four spaces
+    print("x is 1.")
+elif x==2:
+    # indented four spaces again.
+    print("x value is two.")
